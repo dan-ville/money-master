fb7f54409cf7610a051d08f978986c2546c5c763
Daniel Villegas <daniel.villegas@fsgi.com>
2025-04-20 16:23:15 -0400

    favicon and page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { Metadata } from "next"
 import MortgageCalculator from "./mortgage-calc/page"
 
+export const metadata: Metadata = {
+  title: "Money Master",
+  description: "Learn to speak money",
+  icons: {
+    icon: "favicon.svg",
+  },
+}
 export default function Home() {
   return <MortgageCalculator />
 }
